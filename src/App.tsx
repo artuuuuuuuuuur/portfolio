9453cd3ae3cbd5b2d1664fe54bdfc22f5c413297
Artur de Maria <arturdemaria7@gmail.com>
2024-07-25 01:08:44 -0300

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center h-screen">
+        <div className="self-center">
+          <h1 className="text-green-600 font-bold text-xl">Hello World</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
