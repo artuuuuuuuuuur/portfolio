ff3fcaf05d541f6f5e33dca770d6f152141f9228
Artur de Maria <arturdemaria7@gmail.com>
2024-07-27 03:16:01 -0300

    [SOLVED] token issue

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://api.github.com/users/artuuuuuuuuuur",
-  headers: {
-    Authorization: "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",
-  },
 });
 
 export default api;
